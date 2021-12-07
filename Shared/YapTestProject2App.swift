06abe387de6549633f706a58e33a9fa351e6a4df
Emory Al-Imam <emory@yapstudios.com>
2021-12-07 14:09:36 -0800

    Initial Commit

---
+++ Shared/YapTestProject2App.swift
@@ -0,0 +1,17 @@
+//
+//  YapTestProject2App.swift
+//  Shared
+//
+//  Created by Emory Al-Imam on 12/7/21.
+//
+
+import SwiftUI
+
+@main
+struct YapTestProject2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
